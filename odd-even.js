23caaf066bb52632840a1373a515eb5b07ecaa53
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-08-08 20:16:30 +0600

    odd-even.js added

---
+++ odd-even.js
@@ -0,0 +1,12 @@
+function oddEven(number) {
+    if (number % 2 == 0) {
+        console.log('The number is even');
+    }
+
+    else {
+        console.log('The number is odd');
+    }
+
+}
+const num = 1456;
+const isOddEven = oddEven(num);
